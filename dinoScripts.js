0a077c493c6a34e3421ff7e13e55931fcba11cf8
cyu72 <cyu72@binghamton.edu>
2023-01-02 14:22:30 -0500

    reformatted dinoScripts.js for readability

--- dinoScripts.js
+++ dinoScripts.js
@@ -1,19 +1,15 @@
 const dino = document.getElementById("dino");
-document.body.onkeyup = function(e) {
-    if (e.key == " " ||
-        e.code == "Space" ||      
-        e.keyCode == 32      
-    ) {
-      jump();
-    }
+document.body.onkeyup = function (e) {
+  if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
+    jump();
   }
+};
 
 let jump = () => {
-    if (dino.classList != "jump"){
-
-        dino.classList.add("jump");
-        setTimeout(function(){
-            dino.classList.remove("jump");
-        }, 600);
-    }
-}
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 600);
+  }
+};
