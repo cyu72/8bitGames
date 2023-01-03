1d3227c0f8a1162cbbe4323d2aa3bcbf240058f9
cyu72 <cyu72@binghamton.edu>
2023-01-03 16:55:32 -0500

    added collision to detect game over

--- dinoScripts.js
+++ dinoScripts.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 document.body.onkeyup = function (e) {
   if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
     jump();
@@ -10,6 +11,20 @@ let jump = () => {
     dino.classList.add("jump");
     setTimeout(function () {
       dino.classList.remove("jump");
-    }, 600);
+    }, 350);
   }
 };
+
+let isAlive = setInterval(function () {
+  // check x pos of cactus to x pos of dino
+  // if y pos of dino on y pos of cactus = dead
+  let dinoBottom = parseInt(window.getComputedStyle(dino).getPropertyValue("bottom"));
+  let cactusXpos = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+  
+  if (cactusXpos >= 20 && cactusXpos <= 50){
+    if (dinoBottom < -110 ){
+      alert("Gmae Over!");
+    }
+  }
+
+}, 10);
